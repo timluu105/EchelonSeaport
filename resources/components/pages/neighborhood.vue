b2425c35b33f2e5fc214077c4186f0a1b4748e13
Tim Luu <timluu105@gmail.com>
2018-12-05 18:02:40 -0500

    Add Seaport video to neighborhood page

--- resources/components/pages/neighborhood.vue
+++ resources/components/pages/neighborhood.vue
@@ -47,6 +47,11 @@
 
         <stripe :c-content="neighborhoodGalleryContent" />
         <modal-gallery :c-images="neighborhoodSliderImages" />
+        <stripe :c-content="{width: 700}">
+            <div class='embed-container'>
+                <iframe src="https://www.youtube.com/embed/fsafWusvKig" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </stripe>
     </div>
 </template>
 
@@ -167,3 +172,7 @@
         }
     };
 </script>
+<style scoped>
+    .embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; }
+    .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }
+</style>
--- resources/components/partials/stripe.vue
+++ resources/components/partials/stripe.vue
@@ -2,21 +2,24 @@
     <div class="stripe-component">
         <div class="stripe-component-content">
             <h2
-                v-if="cContent.subtitle !== ''"
+                v-if="('subtitle' in cContent) && cContent.subtitle !== ''"
                 class="stripe-component-content-subtitle"
                 v-html="cContent.subtitle">
             </h2>
 
             <h1
-                v-if="cContent.title !== ''"
+                v-if="('title' in cContent) && cContent.title !== ''"
                 class="stripe-component-content-title"
                 v-html="cContent.title">
             </h1>
 
             <div
-                v-if="cContent.body !== ''"
+                v-if="('body' in cContent) && cContent.body !== ''"
                 class="stripe-component-content-body"
                 v-html="preventDanglingWords(cContent.body)">
+            </div>
+            <div v-if="hasDefaultSlot" :style="inlineComponentBodyStyles" class="stripe-component-content-body">
+                <slot></slot>
             </div>
         </div>
     </div>
@@ -32,8 +35,27 @@
 
         props: {
             cContent: {
-                type: Object
+                type: Object,
+                default: function() {
+                    return {
+                        "width": null
+                    };
+                }
             }
+        },
+        computed: {
+            inlineComponentBodyStyles: function() {
+                if(this.cContent["width"] !== null) {
+                    return {
+                        "max-width": this.cContent["width"] + "px"
+                    };
+                } else {
+                    return {};
+                }
+            },
+            hasDefaultSlot () {
+                return !!this.$slots.default
+            },
         }
     };
 </script>
