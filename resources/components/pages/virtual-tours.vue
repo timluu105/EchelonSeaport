a29006a288572818eeb3c3f0143c63ed91c5c439
Tim Luu <timluu105@gmail.com>
2020-05-08 19:15:45 -0400

    Add virtual tours page

---
+++ resources/components/pages/virtual-tours.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="contact-component">
+        <div class="contact-component-wrapper">
+            <h2>{{ $t("contact.interested") }}</h2>
+            <h5>{{ $t("contact.receive-updates") }}</h5>
+            <virtual-tours />
+        </div>
+    </div>
+</template>
+
+<script>
+    import BasePage from "mixins/base-page.js";
+    import VirtualTours from "sections/virtual-tours.vue";
+
+    export default {
+        mixins: [
+            BasePage
+        ],
+
+        components: {
+            "virtual-tours": VirtualTours
+        }
+    };
+</script>
